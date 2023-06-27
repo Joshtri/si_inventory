012f1d5548c4b19bc76387c869747af091f312b6
dawgselmo15 <dawgselmo15@gmail.com>
2023-06-27 09:08:57 +0800

    cmt from windows

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,8 @@ const crypto = require('crypto');
 const express = require ("express");
 const loginController = require('../controllers/loginController')
 const router = express.Router();
-const loginController = require('../controllers/loginController');
 const db = require("../utils/database");
 
-router.get('/',loginController.loginView);
 
 router.get('/login',loginController.loginAdmin)
 router.get('/reset_pass', loginController.updatePasswordPages)
@@ -14,11 +12,5 @@ router.get('/forget_pass', loginController.forgetPasswordPages)
 
 router.post('/post_login', loginController.adminLogin); 
 router.post('/sending_mail', loginController.sendmail_user);
-
-
-
-router.post('/post_login', async (req, res) => {
-
-});
 
 module.exports=router;
